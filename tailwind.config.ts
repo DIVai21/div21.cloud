cacefabc05f93ba6d47339566721d480532fb203
pato how <patohow@proton.me~>
2026-01-29 13:06:27 -0300

    feat: base institucional DIV21

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        dv21: {
+          background: "06011e",
+          logo: "#66cd00",
+          accent: "#3700FF",
+          cyan: "#00FFDD",
+          navy: "#19048D",
+          dark: "#06011E",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
